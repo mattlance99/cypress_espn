1d34f187f11a1dad1736cef1d89091217fb5813b
github username <github email address>
2019-06-25 10:49:22 -0400

    looking for NFL selection

--- cypress/integration/finding_elements.js
+++ cypress/integration/finding_elements.js
@@ -1,7 +1,9 @@
-describe("Verifies that user can access Espn.com", function() {
-  it("Verifies that espn.com displays", function() {
+describe("Verifies that after entering Wicked the entry appears", function() {
+  it("Enters Wicked in the search for title textbox", function() {
     cy.visit("https://www.espn.com");
-
-    //Learn the new ways to discover elements
+    cy.get("input").type("NFL");
+    cy.contains("NFL").click();
+    //This next step would fail if the result didn't exist
+    cy.url().should("include", "/NFL/");
   });
 });
