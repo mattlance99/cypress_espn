7ceaa960320ff8566ad608f9f3bdf6983e6cc24c
github username <github email address>
2019-06-25 10:44:25 -0400

    removed action file

--- cypress/integration/intitial_test.js
+++ cypress/integration/intitial_test.js
@@ -1,8 +1,8 @@
 describe("Verifies that user can access Espn.com", function() {
   it("Verifies that espn.com displays", function() {
     cy.visit("https://www.espn.com");
+
+    //Added this line as my second commit
     cy.url().should("include", "www.espn.com");
-    //cy.contains("Comedy").click();
-    //cy.url().should("include", "=comedy");
   });
 });
