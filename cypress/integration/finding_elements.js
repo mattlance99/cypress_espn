37173909886375995f143b0f7e8884fbc7b20eb9
github username <github email address>
2019-06-25 10:45:51 -0400

    added a finding_element.js

---
+++ cypress/integration/finding_elements.js
@@ -0,0 +1,7 @@
+describe("Verifies that user can access Espn.com", function() {
+  it("Verifies that espn.com displays", function() {
+    cy.visit("https://www.espn.com");
+
+    //Learn the new ways to discover elements
+  });
+});
